8beafd096fc9abfeda72d979c331cc4005f2bb15
Axel <soulchildsince2013@gmail.com>
2019-02-12 22:59:02 +0800

    Initial Commit

---
+++ AppStoreCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppStoreCard
+//
+//  Created by Soulchild on 12/02/2019.
+//  Copyright © 2019 fluffy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
